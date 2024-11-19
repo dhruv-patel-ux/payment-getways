aff9b7f01677ff6e89f86dea508122a21490dcef
dhruv_patel <117365841+dhruv-patel-ux@users.noreply.github.com>
2024-11-19 14:27:23 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   stripe: {
     publicKey:
-      'pk_test_51JZ9mzSG6ZRgx4Y6qvEBfoNTSBoPGCua4CAZMggnqs4Du0vwcI9NDv8wCiieMozvS0kUO2Dy6QJoSpnxkN6yJm6700rFazIQdI',
+      'your strip public key',
   },
 };
